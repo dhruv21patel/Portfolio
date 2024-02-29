4d85f9d524c1433a0f50234fe500e3a8f47e9faf
dhruv211 <dhruv.21patel01@gmail.com>
2024-02-29 10:44:17 -0800

    portfolio
    still have to complete the journey page

---
+++ main.js
@@ -0,0 +1,14 @@
+
+// let swipercard = new Swiper(".swipe-content", {
+//   effect: "flip",
+//   loop: True,
+//   spaceBetween: 32,
+//   grabCursor: true,
+//   pagination: {
+//     el: ".swiper-pagination",
+//   },
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
